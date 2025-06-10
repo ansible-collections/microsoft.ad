f99559244f108cb50d033b2ec7dd599de54fe6ce
Yannik Sembritzki <yannik@sembritzki.org>
2025-06-10 18:09:45 +0200

    Fix microsoft.ad.object_info returning multi valued attributes with only one entry as string instead of array (fixes #199)

--- plugins/modules/object_info.ps1
+++ plugins/modules/object_info.ps1
@@ -298,9 +298,11 @@ $module.Result.objects = @(foreach ($adId in $foundGuids) {
 
             $value = $adObject.$name
             if ($value -is [Microsoft.ActiveDirectory.Management.ADPropertyValueCollection]) {
-                $value = foreach ($v in $value) {
-                    ConvertTo-OutputValue -InputObject $v
+                $value_tmp = @()
+                foreach ($v in $value) {
+                    $value_tmp += ConvertTo-OutputValue -InputObject $v
                 }
+                $value = $value_tmp
             }
             else {
                 $value = ConvertTo-OutputValue -InputObject $value
