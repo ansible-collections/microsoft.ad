807c34b2f88d3e10ecfe635ff878ee082533dd21
Shahar Golshani <sgolshan@redhat.com>
2026-03-22 22:45:41 +0200

    add kds root key module (#224)
    * add kds root key module
    
    * extend action plugin documentation to complete attributes
    
    * update key check
    
    * update test
    
    * update new root key output
    
    * get correct key id in module output
    
    * use datetime format in filter
    
    * test
    
    * update
    
    * switch sa test to use new module
    
    * update
    
    * update
    
    * update service account docs to reference the new module
    
    * fix line length
    
    * rever adobject util
    
    * remove changelog
    
    * add info module
    
    * update root key module with feedback
    
    * remove ad auth doc fragment
    
    * fix test deps
    
    * remove unused method
    
    * Code Review Fixes
    
    * Update plugins/modules/kds_root_key_info.yml
    
    Co-authored-by: Jordan Borean <jborean93@gmail.com>
    
    * Update plugins/modules/kds_root_key.ps1
    
    Co-authored-by: Jordan Borean <jborean93@gmail.com>
    
    * Retrigger softwarefactory-project-zuul
    
    ---------
    
    Co-authored-by: Mike Morency <mikemorency93@gmail.com>
    Co-authored-by: Jordan Borean <jborean93@gmail.com>

---
+++ plugins/modules/kds_root_key.ps1
@@ -0,0 +1,105 @@
+#!powershell
+
+# Copyright (c) 2026 Ansible Project
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+#AnsibleRequires -CSharpUtil Ansible.Basic
+
+
+$spec = @{
+    options = @{
+        effective_time_hours = @{
+            type = 'int'
+            default = 10
+        }
+        match_by = @{
+            type = 'str'
+            default = 'any'
+            choices = @('any', 'key_id', 'never')
+        }
+        state = @{
+            type = 'str'
+            default = 'present'
+            choices = @('present', 'absent')
+        }
+        key_id = @{
+            type = 'str'
+        }
+    }
+    supports_check_mode = $true
+    required_if = @(
+        , @('state', 'absent', @('key_id'))
+        , @('match_by', 'key_id', @('key_id'))
+    )
+}
+
+
+$module = [Ansible.Basic.AnsibleModule]::Create($args, $spec)
+$module.Result.key_id = $null
+$module.Result.changed = $false
+
+$effective_time_hours = $module.Params.effective_time_hours
+$state = $module.Params.state
+$key_id = $module.Params.key_id
+$match_by = $module.Params.match_by
+
+# Note: The EffectiveImmediately parameter does not seem to work as expected.
+# https://learn.microsoft.com/en-us/answers/questions/441587/i-ran-add-kdsrootkey-effectiveimmediately-and-now
+$effctive_time_cmdlet = $effective_time_hours - 10
+$effctive_time_cmdlet_param = @{ EffectiveTime = (Get-Date).AddHours($effctive_time_cmdlet) }
+
+try {
+    $existing_kds_keys = Get-KdsRootKey
+}
+catch {
+    $module.FailJson("Failed to get KDS root keys: $_", $_)
+}
+
+if ($state -eq "present") {
+    if ($match_by -eq "any" -and $existing_kds_keys) {
+        $module.Result.key_id = [Guid]::Empty
+    }
+    elseif ($match_by -eq "key_id" -and $existing_kds_keys.KeyId -contains $key_id) {
+        $module.Result.key_id = $key_id
+    }
+    else {
+        $module.Result.changed = $true
+        if (-not $module.CheckMode) {
+            try {
+                $new_key = Add-KdsRootKey @effctive_time_cmdlet_param
+                $module.Result.key_id = $new_key
+            }
+            catch {
+                $module.FailJson("Failed to create KDS root key: $_", $_)
+            }
+        }
+        else {
+            $module.Result.key_id = [Guid]::Empty
+        }
+    }
+}
+else {
+    $dse = Get-ADRootDSE
+    $ldap_filter = "(&(objectClass=msKds-ProvRootKey)(name=$key_id))"
+    $search_base = "CN=Master Root Keys,CN=Group Key Distribution Service,CN=Services,$($dse.configurationNamingContext)"
+    try {
+        $key_result = Get-ADObject -ldapfilter $ldap_filter -SearchBase $search_base
+    }
+    catch {
+        $module.FailJson("Failed to lookup KDS root keys for removal: $_", $_)
+    }
+    $module.Result.key_id = $key_id
+    if ($null -ne $key_result) {
+        $module.Result.changed = $true
+        if (-not $module.CheckMode) {
+            try {
+                $key_result | Remove-ADObject -Confirm:$False
+            }
+            catch {
+                $module.FailJson("Failed to remove KDS root key: $_", $_)
+            }
+        }
+    }
+}
+
+$module.ExitJson()
---
+++ plugins/modules/kds_root_key_info.ps1
@@ -0,0 +1,61 @@
+#!powershell
+
+# Copyright (c) 2026 Ansible Project
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+#AnsibleRequires -CSharpUtil Ansible.Basic
+#AnsibleRequires -PowerShell Ansible.ModuleUtils.AddType
+
+
+$spec = @{
+    options = @{
+        key_id = @{
+            type = 'str'
+        }
+    }
+    supports_check_mode = $true
+}
+
+
+$module = [Ansible.Basic.AnsibleModule]::Create($args, $spec)
+$module.Result.changed = $false
+$module.Result.kds_root_keys = @()
+
+$search_key_id = $module.Params.key_id
+
+
+try {
+    foreach ($key in $(Get-KdsRootKey)) {
+        # Syntax: UTC Coded Time - .NET DateTimes serialized as in the form "Date(FILETIME)" which isn't easily
+        # parsable by Ansible, instead return as an ISO 8601 string in the UTC timezone.
+        # Matches the time format in the object info module
+        $time = [TimeZoneInfo]::ConvertTimeToUtc($key.EffectiveTime).ToString("o")
+        $creation_time = [TimeZoneInfo]::ConvertTimeToUtc($key.CreationTime).ToString("o")
+        $version_number = $key.VersionNumber
+        $found_key_id = $key.KeyId.Guid.ToString()
+        $found_key = @{
+            key_id = $found_key_id
+            effective_time = $time
+            creation_time = $creation_time
+            version_number = $version_number
+        }
+
+        if ((-not [string]::IsNullOrWhiteSpace($search_key_id))) {
+            # only emit the key that matches the specified ID
+            if ($search_key_id -eq $found_key_id) {
+                $module.Result.kds_root_keys += $found_key
+                break
+            }
+        }
+        else {
+            # No search key specfied, emit all keys
+            $module.Result.kds_root_keys += $found_key
+        }
+    }
+}
+catch {
+    $module.FailJson("Failed to get KDS root keys: $_", $_)
+}
+
+
+$module.ExitJson()
